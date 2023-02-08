7d9c22ef66ac394fa6d66751ed75f6346e5c7679
Julia <iulia.ryseva@gmail.com>
2023-02-08 12:20:33 +0300

    change link

--- src/utils/sendDataToForms.js
+++ src/utils/sendDataToForms.js
@@ -1,7 +1,5 @@
-const GOOGLE_FORM_EMAIl_ID = 'entry.787520719';
-const GOOGLE_FORM_ACTION_URL = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfxT8ngFQ7Qs7woijuHM4_Zcua-NW9IS9g8Tz7V4u1CuNLSZA/formResponse';
-// const GOOGLE_FORM_ACTION_URL = 'https://docs.google.com/forms/u/1/d/e/1FAIpQLScn40Wmhtd3TFdIipCLvvvJykOkING84-Pocn2I7-ObQeadSQ/formResponse';
-// const GOOGLE_FORM_EMAIl_ID = 'entry.752027069';
+const GOOGLE_FORM_ACTION_URL = 'https://docs.google.com/forms/u/1/d/e/1FAIpQLScn40Wmhtd3TFdIipCLvvvJykOkING84-Pocn2I7-ObQeadSQ/formResponse';
+const GOOGLE_FORM_EMAIl_ID = 'entry.752027069';
 
 export const sendDataToForms = ({mail}) => {
     const formData = new FormData();
@@ -14,7 +12,7 @@ export const sendDataToForms = ({mail}) => {
     const myRequest = new Request(GOOGLE_FORM_ACTION_URL, myInit);
     return fetch(myRequest).then(response => {
         return response
-    }).catch(e => {
-        return;
+    }).catch(() => {
+        return {error: true};
     })
 };
